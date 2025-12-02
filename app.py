7a4e379debea2b8f8c068f37fd18a17bc575fbb6
cyb3erasad <cyb3rasad@gmail.com>
2025-12-02 23:01:11 +0500

    Add user authentication

--- app.py
+++ app.py
@@ -1,32 +1,85 @@
 from flask import Flask, render_template, redirect, request
-from models import db, Task
+from flask_login import LoginManager, login_required, logout_user, login_user, current_user
+from flask_bcrypt import Bcrypt
+from models import db, Task, User
+
 
 app = Flask(__name__)
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///todo.db"
+app.config["SECRET_KEY"] = "e2f9a6d4c583"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db.init_app(app)
 
+bcrypt = Bcrypt(app)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "login"
+
 with app.app_context():
     db.create_all()
 
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        hashed_pw = bcrypt.generate_password_hash(password).decode('utf-8')
+
+        new_user = User(username = username, password = hashed_pw)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect("/login")
+    return render_template("register.html")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        user = User.query.filter_by(username=username).first()
+
+        if user and bcrypt.check_password_hash(user.password, password):
+            login_user(user)
+            return redirect("/")
+    return render_template("login.html")
+          
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect("/login")
+
+
 
 @app.route("/")
+@login_required
 def index():
-    tasks = Task.query.all()
+    tasks = Task.query.filter_by(user_id=current_user.id).all()
     return render_template("index.html", tasks=tasks)
 
 @app.route("/add", methods=["POST"])
+@login_required
 def add_task():
     title = request.form.get("title")
-    new_task = Task(title=title)
+    new_task = Task(title=title, user_id=current_user.id)
     db.session.add(new_task)
     db.session.commit()
 
     return redirect("/")
 
 @app.route("/delete/<int:id>")
+@login_required
 def delete_task(id):
     task = Task.query.get(id)
     db.session.delete(task)
@@ -34,6 +87,7 @@ def delete_task(id):
     return redirect("/")
 
 @app.route("/complete/<int:id>")
+@login_required
 def complete_task(id):
     task = Task.query.get(id)
     task.complete = True
--- models.py
+++ models.py
@@ -1,5 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_login import UserMixin
 db = SQLAlchemy()
 
 class Task(db.Model):
@@ -7,3 +7,10 @@ class Task(db.Model):
     title = db.Column(db.String(300), nullable=False)
     complete = db.Column(db.Boolean, default=False)
 
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
